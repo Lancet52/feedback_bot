9e03bbdb7dabf09fff6ec698957a5afcc1347a2d
LANCETPC <sazonov-vs@mail.ru>
2024-09-01 20:46:11 +0300

    Добавлена возмоность отвечать пользователю файлами

--- app/admin_handlers.py
+++ app/admin_handlers.py
@@ -8,7 +8,8 @@ router_adminhandlers = Router()
 
 
 @router_adminhandlers.message(F.reply_to_message)
-async def reply_to_user(message: Message):
+
+async def reply_to_user_text(message: Message):
     entities = message.reply_to_message.entities or message.reply_to_message.caption_entities
     if not entities or entities[0].type != "hashtag":
         raise ValueError("Не удалось извлечь ID для ответа!")
@@ -19,4 +20,14 @@ async def reply_to_user(message: Message):
             raise ValueError("Некорректный ID для ответа!")
         else:
             user_id = hashtag[3:]
-            await message.bot.send_message(chat_id=user_id, text=message.text)
+            if message.text:
+                await message.bot.send_message(chat_id=user_id, text=message.text)
+            elif message.photo:
+                await message.bot.send_photo(chat_id=user_id, photo=message.photo[-1].file_id, caption=message.caption)
+            elif message.video:
+                await message.bot.send_video(chat_id=user_id, video=message.video.file_id, caption=message.caption)
+            elif message.document:
+                await message.bot.send_document(chat_id=user_id, document=message.document.file_id, caption=message.caption)
+            else:
+                print('Данный формат файла не поддерживается!')
+
